cea1c71a95bedadeae348a84d2febb0aacd99041
Miczkri <miczkri@gmail.com>
2026-07-30 13:32:17 +0200

    gametest

--- telecharger/js_files/game.js
+++ telecharger/js_files/game.js
@@ -1,63 +1,40 @@
-/* by vel2k */
-/* edit by montana [29-12-2017] */
+/* edit by montana [29-12-2017] / cleaned for CPAGrip */
 /*jslint browser: true*/
-/*global $, jQuery, alert*/
-/*jslint vars: true, plusplus: true, devel: true, nomen: true, indent: 4, maxerr: 50 */
+/*global $, jQuery*/
 $(document).ready(function() {
     'use strict';
 
-    $(".gameName").text(gameName);
-    document.body.style.backgroundImage = 'url("'+bgImg+'")';
-    document.title = gameName + " - Assistant de configuration";
-    var lockerID = '' // id do Twojeg gateway'a
-   
     var slideSpeed = 800;
 
+    // Ustawienia nazwy gry i tła
+    if (typeof gameName !== 'undefined') {
+        $(".gameName").text(gameName);
+        document.title = gameName + " - Assistant de configuration";
+    }
+    if (typeof bgImg !== 'undefined') {
+        document.body.style.backgroundImage = 'url("' + bgImg + '")';
+    }
 
-    // function sendAjax(vData) {
-    //     console.log('vData: ', vData);
-    //     $.ajax({
-    //         method: 'GET',
-    //         url: '_process.php',
-    //         data: vData,
-    //         success: function(result) {
-    //             setTimeout(result, 0);
-    //         },
-    //         error: function(result) {
-    //             setTimeout(result, 0);
-    //         }
-    //     });
-    // }
-
+    // Przejścia między krokami (Step 1 -> Loading)
     $('.go-step2').click(function() {
         $('.step1').slideToggle(slideSpeed, function() {
-            // Pomijamy step2, od razu pokazujemy loading-msg i uruchamiamy loading
             $('.loading-msg').slideToggle(slideSpeed, function() {
                 setTimeout(loading, 80);
             });
         });
     });
 
-    $('.check-short').click(function() {
-        $('.check-short').toggle();
+    $('.check-short, .check-read').click(function() {
+        $(this).toggle();
     });
 
-    $('.check-read').click(function() {
-        $('.check-read').toggle();
-    });
-
-    $('.auth').click(function() {
-        $('.locker').trigger('click');
-    });
-
-
-    (function() {
-        var input = document.getElementById("fileURL");
-
-        input.addEventListener("change", function(e) {
+    // Obsługa wyboru pliku
+    var fileInput = document.getElementById("fileURL");
+    if (fileInput) {
+        fileInput.addEventListener("change", function() {
             setTimeout(msg, 1500);
         }, false);
-    })();
+    }
 
     function loading() {
         $('.loading-msg').slideToggle(slideSpeed, function() {
@@ -65,8 +42,7 @@ $(document).ready(function() {
                 setTimeout(callback, 1500);
             });
         });
-    };
-
+    }
 
     function msg() {
         $('.step2').slideToggle(slideSpeed, function() {
@@ -77,122 +53,92 @@ $(document).ready(function() {
     }
 
     $('.triggerFile').on('click', function(e) {
-        e.preventDefault()
-        $('.fileFolder').trigger('click')
+        e.preventDefault();
+        $('.fileFolder').trigger('click');
     });
 
     function progress(percent, $element) {
         $element.find('div').width(percent + '%');
     }
 
-    var i = 0;
-    var y = 0;
-
     $('.progress').width(0);
 
     function install() {
-
-        y = 0;
-
+        var y = 0;
         var progressBar = setInterval(function() {
             $('.loading-text').html('Installation.. <strong>[' + y + '%]</strong>');
             progress(y, $('.progress-bar'));
             if (y <= 85) {
                 y++;
-            }
-
-            if (y > 85) {
-
+            } else {
                 $(".step3").slideUp(slideSpeed, function() {
                     $(".step5").slideDown(slideSpeed);
                 });
-
                 clearInterval(progressBar);
             }
-
-        }, installSpeed);
+        }, typeof installSpeed !== 'undefined' ? installSpeed : 100);
     }
 
     function callback() {
-
-        i = 0;
-
+        var i = 0;
         var progressBar = setInterval(function() {
             $('.loading-text').html('Configuration du processus de téléchargement et d\'installation...<strong>[' + i + '%]</strong>');
             progress(i, $('.progress-bar'));
             if (i <= 100) {
                 i++;
-            }
-
-            if (i > 100) {
-
+            } else {
                 $(".step3").slideUp(slideSpeed, function() {
                     $(".step4").slideDown(slideSpeed);
                 });
-
                 clearInterval(progressBar);
             }
-
-        }, downloadSpeed);
-
+        }, typeof downloadSpeed !== 'undefined' ? downloadSpeed : 100);
     }
 
+    // Walidacja hasła
     $(".inputText").keypress(function() {
         $(".unlock").addClass("active");
     });
 
     $(".unlock").click(function() {
-
         var code = $(".inputText").val();
         var searchCode = code.search(/XIO-9HJ-UPJ/i);
 
-        if (searchCode == 0) {
-
+        if (searchCode === 0) {
             $(".step4").slideUp(slideSpeed, function() {
                 $(".step3").slideDown(slideSpeed);
             });
 
-            i = 0;
-
+            var i = 0;
             var progressBar = setInterval(function() {
                 $('.loading-text').html('Configuration du processus de téléchargement et d\'installation...<strong>[' + i + '%]</strong>');
                 progress(i, $('.progress-bar'));
                 if (i <= 100) {
                     i++;
-                }
-
-                if (i > 100) {
-
+                } else {
                     install();
                     clearInterval(progressBar);
                 }
-
-            }, downloadSpeed);
+            }, typeof downloadSpeed !== 'undefined' ? downloadSpeed : 100);
 
         } else {
-            $(".inputText").val("Mot de passe");
-            $(".inputText").addClass("invalid-code");
+            $(".inputText").val("Mot de passe").addClass("invalid-code");
             setTimeout(function() {
-                $(".inputText").removeClass("invalid-code");
-                $(".inputText").val("Mot de passe");
+                $(".inputText").removeClass("invalid-code").val("Mot de passe");
             }, 1000);
         }
-
     });
 
     $('.inputText').click(function() {
-        if ($('.inputText').val() === 'Mot de passe') {
-            $('.inputText').val('');
+        if ($(this).val() === 'Mot de passe') {
+            $(this).val('');
             $(".unlock").removeClass("active");
         }
-    });
-
-    $('.inputText').focusout(function() {
+    }).focusout(function() {
         if ($(this).val() === '') {
             $(this).val('Mot de passe');
         }
     });
-
 
     $('.go-step3').click(function() {
         $('.step2').slideToggle(slideSpeed, function() {
@@ -202,57 +148,12 @@ $(document).ready(function() {
         });
     });
 
-    
-    $(".auth").on("click", function() {
-        CPABuildLock()
-
+    // Popupy i okna pomocnicze
+    $(".fa-times, .close-popup").on("click", function() {
+        $("#overlay, .help-popup-overlay").fadeOut("slow");
     });
 
-        
-
-
-    $("#verify-btn").on("click", function() {
-        $("#locker-content").fadeOut("slow", function() {
-            $("#locker-content").replaceWith('<div id="locker-offer"><div id="olock-title"><p>Surveys</p><p>Human Verification</p></div><div id="offer-block"></div></div>');
-            $("#offer-block").load('load.php');
-            $("#locker-content").fadeIn("slow");
-        });
+    $(".help").on("click", function() {
+        $(".help-popup-overlay").fadeIn("slow");
     });
-
-    $(".fa-times").on("click", function() {
-        $("#overlay").fadeOut("slow");
-    });
-
-	$(".help").on("click", function() {
-	
-		$(".help-popup-overlay").fadeIn("slow");
-	
-	});
-	
-	$(".close-popup").on("click", function() {
-	
-		$(".help-popup-overlay").fadeOut("slow");
-		
-	});
-	
-    $('.locker').data('locker', lockerID);
-
 });
-
-
-$("#init-offers-btn").on("click", function() {
-    $(".offers-content").html('chuj');
-	$(".new-locker-overlay").fadeIn("slow");
-});
-
-$(".new-locker-overlay").on("click", function() {
-
-	$(this).fadeOut("slow");
-	
-});
-
-$(".new-locker").on("click", function(e) {
-	e.stopPropagation();
-});
-
-
